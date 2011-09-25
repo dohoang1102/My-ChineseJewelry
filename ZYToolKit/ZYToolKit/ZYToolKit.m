b2a673c1ddef00e20a8af86106f5003922748a96
Zhao Zhiyang <easycat@ZhaomatoMacBook-Air.local>
2011-09-25 12:23:49 +0800

    提交ZYToolKit

---
+++ ZYToolKit/ZYToolKit/ZYToolKit.m
@@ -0,0 +1,23 @@
+//
+//  ZYToolKit.m
+//  ZYToolKit
+//
+//  Created by Zhiyang Zhao on 11-9-25.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ZYToolKit.h"
+
+@implementation ZYToolKit
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
